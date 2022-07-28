d7bb6e49805b37d0b0cf6077ed2d72a12dede6bd
Wongi <hpurs95@gmail.com>
2022-07-28 19:51:06 +0900

    Initial Commit

---
+++ optioanlANDguard/optioanlANDguardApp.swift
@@ -0,0 +1,17 @@
+//
+//  optioanlANDguardApp.swift
+//  optioanlANDguard
+//
+//  Created by MacBook Air on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct optioanlANDguardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
